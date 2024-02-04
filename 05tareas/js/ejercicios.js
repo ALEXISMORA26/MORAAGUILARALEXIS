92e2a75c90058d6da81d2b0ed9c4b95cb91973c6
ALEXISMORA26 <moraaguilaralexis26@gmail.com>
2024-02-04 15:27:50 -0600

    ejercicio 5

--- 05tareas/js/ejercicios.js
+++ 05tareas/js/ejercicios.js
@@ -78,28 +78,25 @@ function borrarDatosEjercicio4(){
 }
 //
 
-function descuentosCalficacion(){
-    var valorCal = parseInt(document.formulario.cal1.value) + parseInt(document.formulario.cal2.value) + parseInt(document.formulario.cal3.value);
-    console.log("calificaciones: " + valorCal);
-    var valorExam = parseInt(document.formulario.calExamen.value);
-    console.log("examen: " + valorExam);
-    var valorTrabajo = parseInt(document.formulario.calTrabajo.value);
-    console.log("trabajo: " + valorTrabajo);
+function alumnos(){
+    var totalAlumnos = parseInt(document.formulario.mujeres.value) + parseInt(document.formulario.hombres.value);
+    console.log("total alumnos" + totalAlumnos);
+    var totalHombres = parseInt(document.formulario.hombres.value);
+    console.log("total alumnos" + totalHombres);
+    var totalMujeres = parseInt(document.formulario.mujeres.value);
+    console.log("total alumnos" + totalMujeres);
 
     
-    var promCal = valorCal/3 * .55;
-    var promExam = valorExam * .30;
-    var promTrabajo = valorTrabajo * .15;
-    var total =  promCal + promExam + promTrabajo;
+    var promHombres = (totalHombres/totalAlumnos) * 100;
+    var promMujeres = (totalMujeres/totalAlumnos) * 100;
 
-    document.formulario.sueldoI.value = total;
+    document.formulario.sueldoI.value = promHombres + "%";
+    document.formulario.sueldoII.value = promMujeres + "%";
 }
 
-function borrarDatosEjercicio4(){
-    document.formulario.cal1.value = "";
-    document.formulario.cal2.value = "";
-    document.formulario.cal3.value = "";
-    document.formulario.calExamen.value = "";
-    document.formulario.calTrabajo.value = "";
+function borrarDatosEjercicio5(){
+    document.formulario.mujeres.value = "";
+    document.formulario.hombres.value = "";
     document.formulario.sueldoI.value = "";
+    document.formulario.sueldoII.value = "";
 }
